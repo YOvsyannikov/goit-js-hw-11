5c839a4a4de7b8e4a78d432a837fccbefab74ad2
Yuriy Ovsyannikov <yovsyannikov@gmail.com>
2022-08-28 17:06:10 +0300

    Try again

---
+++ src/fetchPhotos.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { Notify } from 'notiflix';
+
+export default class PhotosAPIServise {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.BASE_URL = 'https://pixabay.com/api/';
+    this.KEY = '29560891-792771529aa052fb706988eec';
+  }
+
+  async fetchPhotos() {
+    try {
+      const response = await axios.get(
+        `${this.BASE_URL}?key=${this.KEY}&q=${this.searchQuery}&orientation=horizontal&image_type=photo&safesearch=true&page=${this.page}&per_page=40`
+      );
+      const data = await JSON.parse(response.request.response);
+      return data;
+    } catch (error) {
+      Notify.warning(
+        `We're sorry, but you've reached the end of search results.`
+      );
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  get pageNumber() {
+    return this.page;
+  }
+
+  set pageNumber(newPage) {
+    this.page = newPage;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,130 @@
+import './css/styles.css';
+import PhotosAPIServise from './fetchPhotos';
+import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  div: document.querySelector('.gallery'),
+  moreBtn: document.querySelector('.load-more'),
+};
+
+const photosAPIServise = new PhotosAPIServise();
+
+const submitHandler = e => {
+  e.preventDefault();
+  firstLoadPhotos(e);
+  setTimeout(smoothScroll, 500);
+};
+let gallery;
+const renderGallery = markup => {
+  refs.div.insertAdjacentHTML('beforeend', markup);
+
+  gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+  lastPhotoObserver.observe(document.querySelector('.photo-card:last-child'));
+};
+
+const createGalleryMarkup = data => {
+  return data
+    .map(
+      item =>
+        `
+    <div class="photo-card">
+    <a class="link" href="${item.largeImageURL}">
+      <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" /></a>
+    <div class="info">
+    <p class="info-item">
+      <b>Likes </b>${item.likes}
+    </p>
+    <p class="info-item">
+      <b>Views </b>${item.views}
+    </p>
+    <p class="info-item">
+      <b>Comments </b>${item.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads </b>${item.downloads}
+    </p>
+  </div>
+</div>
+`
+    )
+    .join('');
+};
+
+refs.form.addEventListener('submit', submitHandler);
+
+function loadMorePhotos() {
+  photosAPIServise.incrementPage();
+  photosAPIServise.fetchPhotos().then(data => {
+    const markup = createGalleryMarkup(data.hits);
+    renderGallery(markup);
+  });
+  gallery.refresh();
+  setTimeout(function () {
+    const { height: cardHeight } =
+      refs.div.firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }, 500);
+}
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5,
+};
+
+const lastPhotoObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const lastPhoto = entry.target;
+      loadMorePhotos();
+      lastPhotoObserver.unobserve(lastPhoto);
+    }
+  });
+}, options);
+
+const arr = document.querySelectorAll('img');
+arr.forEach(i => {
+  observer.observe(i);
+});
+
+function firstLoadPhotos(e) {
+  refs.div.innerHTML = '';
+  photosAPIServise.searchQuery = e.target.elements.searchQuery.value.trim();
+
+  if (photosAPIServise.searchQuery.length === 0) {
+    Notify.failure('Please, enter some text');
+  } else {
+    photosAPIServise.resetPage();
+    photosAPIServise.fetchPhotos().then(data => {
+      if (data.hits.length === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        const markup = createGalleryMarkup(data.hits);
+        renderGallery(markup);
+      }
+    });
+  }
+}
+
+function smoothScroll() {
+  const { height: cardHeight } =
+    refs.div.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
